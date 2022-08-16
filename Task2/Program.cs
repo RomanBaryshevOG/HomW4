0bfea7d2f6546b44a7b92b08f37d1a67b3b139ac
RomanBaryshevOG <romanbaryshev.y@gmail.com>
2022-08-16 15:59:17 +0500

    Add Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов
+// и выводит их на экран.
+
+
+int[] NewArray()
+{
+int[] array = new int[8];
+for (int i = 0; i <= 7; i++)
+{
+    array[i] = new Random().Next(0, 1000); // сделал до 1000 
+}
+
+
+    return array;
+}
+
+int[] array = NewArray();
+
+for (int i = 0; i <= 7; i++)
+{
+    Console.Write($" {array [i]}, ");
+}
